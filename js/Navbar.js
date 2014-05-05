fff5f9a4eb61c5577e57b4cd124400d51eed77cc
David Shapiro <dgs700@gmail.com>
2014-05-05 00:19:58 -0700

    nav bar functionality additions- add/remove, ngSanitize

--- js/Navbar.js
+++ js/Navbar.js
@@ -62,115 +62,162 @@
             };
         })
 
-        // Navigation Bar Container Component
-        .directive('uicNavBar', [
-            'uicDropdownService',
-            'uicNavBarService',
-            '$location',
-            '$compile', function( uicDropdownService, uicNavBarService, $location, $compile){
-                return {
-                    template: navbarTpl,
-                    restrict: 'E',
+// Navigation Bar Container Component
+.directive('uicNavBar', [
+    'uicDropdownService',
+    'uicNavBarService',
+    '$location',
+    '$compile', function( uicDropdownService, uicNavBarService, $location, $compile){
+        return {
+            template: navbarTpl,
+            restrict: 'E',
 
-                    // allow page designer to include dropdown elements
-                    transclude: true,
-                    replace: true,
-                    // isolate scope
-                    scope: {
-                        // attribute API for hiding dropdowns
-                        minimalHeader: '@minimal',
-                        homeUrl: '@'
-                    },
-                    controller: [
-                        '$scope',
-                        '$element',
-                        '$attrs', function($scope, $element, $attrs){
+            // allow page designer to include dropdown elements
+            transclude: true,
+            replace: true,
+            // isolate scope
+            scope: {
+                // attribute API for hiding dropdowns
+                minimalHeader: '@minimal',
+                homeUrl: '@'
+            },
+            controller: [
+                '$scope',
+                '$element',
+                '$attrs', function($scope, $element, $attrs){
 
-                        // at the mobile width breakpoint
-                        // the Nav Bar items are not initially visible
-                        $scope.isCollapsed = true;
+                 // make sure $element is updated to the compiled/linked version
+                 var that = this;
+                 this.init = function( element ) {
+                     that.$element = element;
+                 };
 
-                        // menu json data if available
-                        $scope.menus = uicNavBarService.getMenus();
+                // add a dropdown to the nav bar during runtime
+                // i.e. upon hash navigation
+                this.addDropdown = function(menuObj){
 
-                        // keep track of added dropdowns
-                        // for container level manipulation if needed
-                        $scope.registeredMenus = [];
+                    // create an isolate scope instance
+                    var newScope = $scope.$root.$new();
 
-                        // listen for minimize event
-                        $scope.$on('header-minimize', function(evt){
-                            $scope.minimalHeader = true;
+                    // attach the json obj data at the same location
+                    // as the dropdown controller would
+                    newScope.$parent.menu = menuObj;
+
+                    // manually compile and link a new dropdown component
+                    var $el = $compile('<uic-dropdown-menu></uic-dropdown-menu>')(newScope);
+
+                    // attach the new dropdown to the end of the first child <ul>
+                    // todo - add more control over DOM attach points
+                    $element.find('ul').last().append( $el );
+                };
+
+                // remove a dropdown from the nav bar during runtime
+                // i.e. upon hash navigation
+                this.removeDropdown = function(dropdownId){
+
+                    // get a reference to the target dropdown
+                    var menuArray = $scope.registeredMenus.filter(function (el){
+                        return el.uicId == dropdownId;
+                    });
+                    var dropdown = menuArray[0];
+
+                    // remove and destroy it and all children
+                    uicDropdownService.remove(dropdown);
+                    dropdown.iElement.remove();
+                    dropdown.$destroy();
+                };
+
+                    // check for single or array of dropdowns to add
+                // available on scope for additional invokation flexability
+                $scope.addOrRemove = function(dropdowns, action){
+                    action = action + 'Dropdown';
+                    if(angular.isArray(dropdowns)){
+                        angular.forEach(dropdowns, function(dropdown){
+                            that[action](dropdown);
                         });
-
-                        // listen for maximize event
-                        $scope.$on('header-maximize', function(evt){
-                            $scope.minimalHeader = false;
-                        });
-
-                        // listen for dropdown open event
-                        $scope.$on('dropdown-opened', function(evt){
-                            // perform an action when a child dropdown is opened
-                        });
-
-                        // listen for dropdown close event
-                        $scope.$on('dropdown-closed', function(evt){
-                            // perform an action when a child dropdown is closed
-                        });
-
-                        // listen for menu item event
-                        $scope.$on('menu-item-selected', function(evt, scope){
-                            // grab the url string from the menu iten scope
-                            var url;
-                            try{
-                                url = scope.url || scope.item.url;
-                                // handle navigation programatically
-                                $location.path(url);
-                            }catch(err){
-                                //$console.log('no url')
-                            }
-                        });
-                    }],
-                    link: function(scope, iElement, iAttrs, ctrl, $transclude){
-
-                        // know who the tenants are
-                        // note that this link function executes *after*
-                        // the link functions of any inner components
-                        // at this point we could extend our NavBar component
-                        // functionality to rebuild menus based on new json or
-                        // disable individual menu items based on $location
-                        scope.registeredMenus = uicDropdownService.getDropdowns();
-
-                        // Attr API option for sticky vs fixed
-                        scope.position = (iAttrs.sticky == 'true') ? 'navbar-fixed-top' : 'navbar-static-top';
-                        scope.theme = (iAttrs.theme) ? iAttrs.theme : null;;
-//////// proto code for add dropdown option ////////////////
-                        var menu = {"Test":[
-                            {
-                                "text":"Contact Us",
-                                "url":"/company/contact"
-                            },{
-                                "text":"Jobs",
-                                "url":"/company/jobs"
-                            },{
-                                "text":"Privacy Statement",
-                                "url":"/company/privacy"
-                            },{
-                                "text":"Terms of Use",
-                                "url":"/company/terms"
-                            }]
-                        };
-
-                        var newScope = scope.$root.$new();
-
-                        newScope.$parent.menu = menu;
-
-                        var $el = $compile('<uic-dropdown-menu></uic-dropdown-menu>')(newScope)
-                        console.warn($el)
-                        iElement.find('ul').append( $el );
-/////////////////////////////
+                    }else{
+                        that[action](dropdowns);
                     }
                 };
-            }]);
+
+
+                    // at the mobile width breakpoint
+                // the Nav Bar items are not initially visible
+                $scope.isCollapsed = true;
+
+                // menu json data if available
+                $scope.menus = uicNavBarService.getMenus();
+
+                // keep track of added dropdowns
+                // for container level manipulation if needed
+                $scope.registeredMenus = [];
+
+                // listen for minimize event
+                $scope.$on('header-minimize', function(evt){
+                    $scope.minimalHeader = true;
+                });
+
+                // listen for maximize event
+                $scope.$on('header-maximize', function(evt){
+                    $scope.minimalHeader = false;
+                });
+
+                // handle request to add dropdown(s)
+                // obj = menu JSON obj or array of objs
+                $scope.$on('add-nav-dropdowns', function(evt, obj){
+                    $scope.addOrRemove(obj, 'add');
+                });
+
+                // handle request to remove dropdown(s)
+                // ids = string or array of strings matching dd titles
+                $scope.$on('remove-nav-dropdowns', function(evt, ids){
+                    $scope.addOrRemove(ids, 'remove');
+                });
+
+                    // listen for dropdown open event
+                $scope.$on('dropdown-opened', function(evt){
+                    // perform an action when a child dropdown is opened
+                });
+
+                // listen for dropdown close event
+                $scope.$on('dropdown-closed', function(evt){
+                    // perform an action when a child dropdown is closed
+                });
+
+                // listen for menu item event
+                $scope.$on('menu-item-selected', function(evt, scope){
+                    // grab the url string from the menu iten scope
+                    var url;
+                    try{
+                        url = scope.url || scope.item.url;
+                        // handle navigation programatically
+                        $location.path(url);
+                    }catch(err){
+                        //$console.log('no url')
+                    }
+                });
+            }],
+            link: function(scope, iElement, iAttrs, navCtrl, $transclude){
+
+                // know who the tenants are
+                // note that this link function executes *after*
+                // the link functions of any inner components
+                // at this point we could extend our NavBar component
+                // functionality to rebuild menus based on new json or
+                // disable individual menu items based on $location
+                scope.registeredMenus = uicDropdownService.getDropdowns();
+
+                // Attr API option for sticky vs fixed
+                scope.position = (iAttrs.sticky == 'true') ? 'navbar-fixed-top' : 'navbar-static-top';
+
+                // get theme css class from attr API if set
+                scope.theme = (iAttrs.theme) ? iAttrs.theme : null;;
+
+                // send compiled/linked element back to ctrl instance
+                navCtrl.init( iElement );
+            }
+        };
+    }]);
 })();
 
 
